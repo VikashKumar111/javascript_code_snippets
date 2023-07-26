e2c5219a4544a611235e55b3c20baf0b766f82de
Vikash Kumar <vikasthakur9310@gmail.com>
2023-07-26 23:48:06 +0530

    add

--- snippets.js
+++ snippets.js
@@ -71,7 +71,23 @@
 
 
 
-let number = 0;
-console.log(number++);
-console.log(++number);
-console.log(number);
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+
+
+
+function checkAge(data) {
+  if (data === { age: 18 }) {
+    console.log('You are an adult!');
+  } else if (data == { age: 18 }) {
+    console.log('You are still an adult.');
+  } else {
+    console.log(`Hmm.. You don't have an age I guess`);
+  }
+}
+
+checkAge({ age: 18 });
+
+

31b8337ecebb5efd9bf77d916f183e24e7e29d5a
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-03 23:54:37 +0530

    add

--- snippets.js
+++ snippets.js
@@ -129,13 +129,20 @@
 
 
 
-const obj = { 1: 'a', 2: 'b', 3: 'c' };
-const set = new Set([1, 2, 3, 4, 5]);
+// const obj = { 1: 'a', 2: 'b', 3: 'c' };
+// const set = new Set([1, 2, 3, 4, 5]);
 
-console.log(obj.hasOwnProperty('1'));
-console.log(obj.hasOwnProperty(1));
-console.log(set.has('1'));
-console.log(set.has(1));
+// console.log(obj.hasOwnProperty('1'));
+// console.log(obj.hasOwnProperty(1));
+// console.log(set.has('1'));
+// console.log(set.has(1));
+
+
+
+
+const obj = { a: 'one', b: 'two', a: 'three' };
+console.log(obj);
+
 
 
 

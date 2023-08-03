7834d116bae22433c01016484794d060d3f1f3e0
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-03 23:51:43 +0530

    add

--- snippets.js
+++ snippets.js
@@ -115,8 +115,31 @@
 
 
 
-const sum = eval('10*10+5');
-console.log(sum);
+// const sum = eval('10*10+5');
+// console.log(sum);
+
+
+
+// var num = 8;
+// var num = 10;
+
+// console.log(num);
+
+
+
+
+
+const obj = { 1: 'a', 2: 'b', 3: 'c' };
+const set = new Set([1, 2, 3, 4, 5]);
+
+console.log(obj.hasOwnProperty('1'));
+console.log(obj.hasOwnProperty(1));
+console.log(set.has('1'));
+console.log(set.has(1));
+
+
+
+
 
 
 

41c2edd66aac04b1e96305e11cd1eb23c1dc5ac9
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-12 23:41:25 +0530

    add

--- snippets.js
+++ snippets.js
@@ -216,18 +216,20 @@
 
 
 
-(() => {
-  let x, y;
-  try {
-    throw new Error();
-  } catch (x) {
-    (x = 1), (y = 2);
-    console.log(x);
-  }
-  console.log(x);
-  console.log(y);
-})();
+// (() => {
+//   let x, y;
+//   try {
+//     throw new Error();
+//   } catch (x) {
+//     (x = 1), (y = 2);
+//     console.log(x);
+//   }
+//   console.log(x);
+//   console.log(y);
+// })();
 
+
+setInterval(() => console.log('Hi'), 1000);
 
 
 

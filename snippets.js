b5d2fb0346a48e6a1bc7f4d886509b12c492bdc6
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-23 23:43:23 +0530

    add

--- snippets.js
+++ snippets.js
@@ -267,17 +267,30 @@
 
 
 
-(() => {
-  let x, y;
-  try {
-    throw new Error();
-  } catch (x) {
-    (x = 1), (y = 2);
-    console.log(x);
-  }
-  console.log(x);
-  console.log(y);
-})();
+// (() => {
+//   let x, y;
+//   try {
+//     throw new Error();
+//   } catch (x) {
+//     (x = 1), (y = 2);
+//     console.log(x);
+//   }
+//   console.log(x);
+//   console.log(y);
+// })();
+
+
+
+
+[[0, 1], [2, 3]].reduce(
+  (acc, cur) => {
+    return acc.concat(cur);
+  },
+  [1, 2],
+);
+
+
+
 
 
 

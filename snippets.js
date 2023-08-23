0e3ac3ac503196a1db99abec7aff042d04eea996
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-23 23:23:47 +0530

    add

--- snippets.js
+++ snippets.js
@@ -229,7 +229,24 @@
 // })();
 
 
-setInterval(() => console.log('Hi'), 1000);
+// setInterval(() => console.log('Hi'), 1000);
+
+
+
+
+
+const person = { name: 'Lydia' };
+
+function sayHi(age) {
+  return `${this.name} is ${age}`;
+}
+
+console.log(sayHi.call(person, 21));
+console.log(sayHi.bind(person, 21));
+
+
+
+
 
 
 

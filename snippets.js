a771cfa5a7044fc0fb214f2841d6a753bffce510
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-28 23:48:49 +0530

    add

--- snippets.js
+++ snippets.js
@@ -1,21 +1,21 @@
-function sayHi() {
-  console.log(name);
-  console.log(age);
-  var name = 'Lydia';
-  let age = 21;
+// function sayHi() {
+//   console.log(name);
+//   console.log(age);
+//   var name = 'Lydia';
+//   let age = 21;
+// }
+
+// sayHi();
+
+
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
 }
 
-sayHi();
-
-
-
-// for (var i = 0; i < 3; i++) {
-//   setTimeout(() => console.log(i), 1);
-// }
-
-// for (let i = 0; i < 3; i++) {
-//   setTimeout(() => console.log(i), 1);
-// }
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}
 
 
 // const shape = {

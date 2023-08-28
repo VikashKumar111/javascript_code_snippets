d2333a9647b14fb7ae593bbfedb754d348f0e4ad
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-28 23:52:41 +0530

    add

--- snippets.js
+++ snippets.js
@@ -18,27 +18,27 @@
 // }
 
 
-const shape = {
-  radius: 10,
-  diameter() {
-    return this.radius * 2;
-  },
-  perimeter: () => 2 * Math.PI * this.radius,
-};
+// const shape = {
+//   radius: 10,
+//   diameter() {
+//     return this.radius * 2;
+//   },
+//   perimeter: () => 2 * Math.PI * this.radius,
+// };
 
-console.log(shape.diameter());
-console.log(shape.perimeter());
-
-
+// console.log(shape.diameter());
+// console.log(shape.perimeter());
 
 
 
-// let c = { greeting: 'Hey!' };
-// let d;
 
-// d = c;
-// c.greeting = 'Hello';
-// console.log(d.greeting);
+
+let c = { greeting: 'Hey!' };
+let d;
+
+d = c;
+c.greeting = 'Hello';
+console.log(d.greeting);
 
 
 

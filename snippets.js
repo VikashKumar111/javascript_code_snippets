f16d3fd68d99da4860e384579fd4a224cbb3ec49
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-28 23:50:16 +0530

    add

--- snippets.js
+++ snippets.js
@@ -9,25 +9,25 @@
 
 
 
-for (var i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1);
-}
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
 
-for (let i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1);
-}
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
 
 
-// const shape = {
-//   radius: 10,
-//   diameter() {
-//     return this.radius * 2;
-//   },
-//   perimeter: () => 2 * Math.PI * this.radius,
-// };
+const shape = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius,
+};
 
-// console.log(shape.diameter());
-// console.log(shape.perimeter());
+console.log(shape.diameter());
+console.log(shape.perimeter());
 
 
 

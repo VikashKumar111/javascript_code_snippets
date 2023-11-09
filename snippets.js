fc4caf7432e2eb8304a0f1259fe3ada62a3d260f
Vikash Kumar <vikasthakur9310@gmail.com>
2023-11-09 23:44:46 +0530

    add

--- snippets.js
+++ snippets.js
@@ -44,13 +44,13 @@
 
 
 
-let a = 3;
-let b = new Number(3);
-let c = 3;
+// let a = 3;
+// let b = new Number(3);
+// let c = 3;
 
-console.log(a == b);
-console.log(a === b);
-console.log(b === c);
+// console.log(a == b);
+// console.log(a === b);
+// console.log(b === c);
 
 
 
